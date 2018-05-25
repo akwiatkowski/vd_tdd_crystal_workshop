81ed8be3e9000502b180dc7a4f02124561b3e387
Aleksander Kwiatkowski <bobikx@poczta.fm>
2018-05-25 13:02:37 +0200

    allow input as single Int32

--- src/tdd/main.cr
+++ src/tdd/main.cr
@@ -1,12 +1,30 @@
 module Tdd
+  alias InOutObject = (Array(Int32) | Int32)
+
   class Main
-    def initialize(@in_array : Array(Int32))
+    getter :in_object
+
+    def initialize(@in_object : InOutObject)
     end
 
-    def call : Array(Int32)
-      @in_array.select do |i|
-        i < 5
+    def call
+      if in_object.as?(Array)
+        in_object.as(Array(Int32)).select do |i|
+          check_object(i)
+        end
+      else
+        i = in_object.as(Int32)
+        if check_object(i)
+          i
+        else
+          nil
+        end
       end
+
+    end
+
+    private def check_object(i)
+      i < 5
     end
   end
 end
